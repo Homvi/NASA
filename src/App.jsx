09da8cc9c4299c61e246d414a29c5927ab6c3937
homvi <honvedoadamkj@gmail.com>
2023-08-14 14:01:01 +0200

    change arrow colors

--- src/App.jsx
+++ src/App.jsx
@@ -89,24 +89,24 @@ function App() {
   return (
     <div className="">
       <Navbar />
-      <div className="p-3 md:py-11 min-h-screen bg-base-200 flex flex-col items-center md:justify-center">
+      <div className="p-3 lg:py-11 min-h-screen bg-base-200 flex flex-col items-center lg:justify-center">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-xl md:text-5xl py-5 px-3 md:hidden font-bold text-center">
+          <h1 className="text-xl lg:text-5xl py-5 px-3 lg:hidden font-bold text-center">
             {data.title}
           </h1>
-          <p className=" text-center md:hidden py-3 max-w-xl">{data.date}</p>
-          <div className="flex flex-col md:flex-row">
-            <div className="relative w-screen flex justify-center bg-white md:max-w-lg xl:max-w-xl">
+          <p className=" text-center lg:hidden py-3 max-w-xl">{data.date}</p>
+          <div className="flex flex-col lg:flex-row">
+            <div className="relative w-screen flex justify-center bg-white lg:max-w-lg xl:max-w-xl">
               <div
                 onClick={showPrev}
                 className="h-full bg-black/70  flex justify-center items-center text-white absolute left-0 top-0 w-11 cursor-pointer  rotate-180"
               >
-                <span className="text-3xl pb-4 md:pb-1 text-shadow">&gt;</span>
+                <span className="text-3xl pb-4 lg:pb-1 text-shadow">&gt;</span>
               </div>
               <img
                 onClick={() => setIsModalOpen(true)}
                 src={data.url}
-                className="cursor-pointer md:max-w-lg xl:max-w-xl max-h-sm shadow-2xl"
+                className="cursor-pointer lg:max-w-lg xl:max-w-xl max-h-sm shadow-2xl"
               />
               <div
                 onClick={showNext}
@@ -117,14 +117,14 @@ function App() {
                 </span>
               </div>
             </div>
-            <div className="flex flex-col px-3  items-center md:items-start">
-              <h1 className="text-5xl py-5 hidden md:block font-bold">
+            <div className="flex flex-col px-3  items-center lg:items-start">
+              <h1 className="text-5xl py-5 hidden lg:block font-bold">
                 {data.title}
               </h1>
-              <p className=" hidden md:block py-1 text-left max-w-xl">
+              <p className=" hidden lg:block py-1 text-left max-w-xl">
                 {data.date}
               </p>
-              <p className="py-8 md:py-3 max-w-xl">{data.explanation}</p>
+              <p className="py-8 lg:py-3 max-w-xl">{data.explanation}</p>
             </div>
           </div>
         </div>
